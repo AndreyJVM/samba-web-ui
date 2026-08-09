e844dc3e923cf356e01e7de0198f27fbe73e5f93
Andrey_Vorobev <andrey.vorobev.aqa@gmail.com>
2026-08-09 17:14:01 +0300

    feat: first commit

---
+++ src/main/java/mari/samba_web_ui/SambaWebUiApplication.java
@@ -0,0 +1,13 @@
+package mari.samba_web_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambaWebUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SambaWebUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mari/samba_web_ui/SambaWebUiApplicationTests.java
@@ -0,0 +1,13 @@
+package mari.samba_web_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SambaWebUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
