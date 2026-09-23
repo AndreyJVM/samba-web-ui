4ca6269aebda52ea902cfba8bec0533df36de7fe
Andrey_Vorobev <andrey.vorobev.aqa@gmail.com>
2026-09-23 19:57:32 +0300

    fix(ui): restore Vite base URL mapping for correct asset resolution

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ui/', // Указываем базовый путь для генерации ассетов
   server: {
     port: 5173,
     proxy: {
